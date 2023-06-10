1a0de3ffba5c1f9673df0ba68033d14a7d604872
Manoel Vieira <netinhovieira1801@gmail.com>
2023-06-10 15:14:31 -0300

    Tema da aplicação

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
+import { ThemeProvider } from 'styled-components';
 import { Groups } from './src/screens/Groups';
+import theme from './src/theme';
 
 export default function App() {
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
   );
 }
