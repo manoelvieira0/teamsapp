944def40f062491bc03144e444975ea8034ca348
Manoel Vieira <netinhovieira1801@gmail.com>
2023-06-10 19:00:49 -0300

    Utilizando tema

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import { ThemeProvider } from 'styled-components';
+
 import { Groups } from './src/screens/Groups';
+
 import theme from './src/theme';
 
 export default function App() {
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,12 +2,6 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
     flex: 1;
-    justify-content: center;
-    align-items: center;
-    background-color: #000;
-`;
-
-export const Title = styled.Text`
-color: #fff;
-    font-size: 24px;
+    background-color: ${({theme}) => theme.COLORS.GRAY_300};
+    padding: 24px;
 `;
