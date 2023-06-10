7bd66e749fc33575824dd84b0a2730c3bdbd4960
Manoel Vieira <netinhovieira1801@gmail.com>
2023-06-10 19:25:41 -0300

    Componente de Loading

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
+import theme from './src/theme';
 import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from './src/components/Loading';
 
 import { Groups } from './src/screens/Groups';
 
-import theme from './src/theme';
-
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
